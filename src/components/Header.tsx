06902f37444131c230d0f0b619cbcbc4768283eb
renshimosawa <polaris2760@gmail.com>
2024-08-02 01:59:34 +0900

    見た目できた〜

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="w-screen drop-shadow-md bg-red-50 py-2 fixed top-0 left-0">
+      <div className="max-w-[1100px] m-auto flex justify-between items-center">
+        <p className="text-2xl font-bold text-orange-600">
+          カレンダー登録アプリ
+        </p>
+        <Link className="text-xl underline" href="/">
+          予定を作成
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,119 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Header from "@/components/Header";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { DateTimePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { useState } from "react";
+import Alert from "@mui/material/Alert";
+import dayjs, { Dayjs } from "dayjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [title, setTitle] = useState("");
+  const [startDate, setStartDate] = useState<Dayjs | null>(null);
+  const [endDate, setEndDate] = useState<Dayjs | null>(null);
+  const [location, setLocation] = useState("");
+  const [alertMessage, setAlertMessage] = useState("");
+
+  const handleSubmit = () => {
+    if (!title) {
+      setAlertMessage("タイトルを入力してください");
+      return;
+    }
+    if (!startDate) {
+      setAlertMessage("開始日時を入力してください");
+      return;
+    }
+    if (!endDate) {
+      setAlertMessage("終了日時を入力してください");
+      return;
+    }
+
+    const eventObject = {
+      title,
+      startDate,
+      endDate,
+      location,
+    };
+
+    console.log(eventObject.startDate);
+    setAlertMessage("");
+    // ここでURLを発行するための処理を追加します
+  };
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <Header />
+      <main className={`min-h-screen mt-24 ${inter.className}`}>
+        <div className="text-center">
+          <h1 className="text-4xl font-bold">カレンダー登録してちょうだい！</h1>
+          <p className="text-xl mt-5">
+            友達に
+            <span className="font-bold text-orange-600">
+              「この予定カレンダー入れといて！」
+            </span>
+            <br />
+            このサイトを共有すれば、友達は自分のカレンダーに合わせて、予定をすぐには登録できます。
+          </p>
+
+          <ul className="mt-3">
+            <li>
+              ※
+              URLを生成する性質上、ブラウザ版を提供しているカレンダーにしか対応していません。
+            </li>
+            <li>
+              ※
+              ユーザー登録を実装するのは色々面倒なので、URLパラメーターを読み取って復元します。
+            </li>
+            <li>
+              ※ カレンダー提供元の仕様が変わったらどうしようもありません。
+            </li>
+          </ul>
+
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <div className="mt-10 max-w-[700px] m-auto flex flex-col gap-y-4 justify-center items-center">
+              <TextField
+                className="w-full"
+                required
+                id="event_title"
+                label="STEP 1: タイトル/Title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+              <DateTimePicker
+                className="w-full"
+                label="STEP 2: 開始日時/Start date *"
+                value={startDate}
+                onChange={(newValue) => setStartDate(newValue)}
+              />
+              <DateTimePicker
+                className="w-full"
+                label="STEP 3: 終了日時/End date *"
+                value={endDate}
+                onChange={(newValue) => setEndDate(newValue)}
+              />
+              <TextField
+                className="w-full"
+                id="event_location"
+                label="STEP4 : 場所/Location"
+                value={location}
+                onChange={(e) => setLocation(e.target.value)}
+              />
+              <Button
+                className="mt-4"
+                size="large"
+                variant="contained"
+                onClick={handleSubmit}
+              >
+                URLを発行する
+              </Button>
+              {alertMessage && <Alert severity="error">{alertMessage}</Alert>}
+            </div>
+          </LocalizationProvider>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </main>
+    </div>
   );
 }
